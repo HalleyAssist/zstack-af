f410527c031c82c4f11ae53c53b61d0d30565619
Mathew Heard <splitice@users.noreply.github.com>
2022-12-05 23:57:33 +1100

    fix ota library

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const AfController = require('./lib/afController.js')
 const Af = require('./lib/af.js')
 const AfError = require('./lib/afError')
 const Packet = require('./lib/packet')
-const Ota = require('./lib/Ota')
+const Ota = require('./lib/ota')
 
 module.exports = {AfController, Af, AfError, Packet, Ota}
