e8a99925712851b32f7df4f3c212ebe10c4a04ba
Mathew Heard <splitice@users.noreply.github.com>
2022-12-06 11:24:54 +1100

    capital file name style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const AfController = require('./lib/afController.js')
-const Af = require('./lib/af.js')
-const AfError = require('./lib/afError')
-const Packet = require('./lib/packet')
-const Ota = require('./lib/ota')
+const AfController = require('./lib/AfController.js')
+const Af = require('./lib/Af.js')
+const AfError = require('./lib/AfError')
+const Packet = require('./lib/Packet')
+const Ota = require('./lib/Ota')
 
 module.exports = {AfController, Af, AfError, Packet, Ota}
